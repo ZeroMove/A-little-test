bc7b161c90b04449d58e3b0bbdd37001c0b3a4bf
zeromove <ZeroMove@users.noreply.github.com>
2019-08-15 18:49:30 +0800

    test

--- ex1.m
+++ ex1.m
@@ -1,26 +1,4 @@
-%%%% Machine Learning Online Class - Exercise 1: Linear Regression
 
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     warmUpExercise.m
-%     plotData.m
-%     gradientDescent.m
-%     computeCost.m
-%     gradientDescentMulti.m
-%     computeCostMulti.m
-%     featureNormalize.m
-%     normalEqn.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-% x refers to the population size in 10,000s
-% y refers to the profit in $10,000s
 %
 %123
 %% Initialization
